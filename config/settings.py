e0c3d54b9870f60957a0f908d521d1a1eeab9f7c
cadiazc-gif <cadiazc@gmail.com>
2026-04-05 20:40:32 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get("SQLITE_PATH", "/app/data/db.sqlite3"),
     }
 }
 
