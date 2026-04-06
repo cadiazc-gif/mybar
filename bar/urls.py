fc7e1e508e86e0e80afef9467c8a2e9b840ea9f1
cadiazc-gif <cadiazc@gmail.com>
2026-04-06 17:00:00 -0400

    Update urls.py

--- bar/urls.py
+++ bar/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("unlock-suggestions/", views.unlock_suggestions, name="unlock_suggestions"),
     path("my-bar/", views.my_bar_dashboard, name="my_bar_dashboard"),
     path("runtime-diagnose-2026/", views.runtime_diagnose, name="runtime_diagnose"),
+    
 ]
